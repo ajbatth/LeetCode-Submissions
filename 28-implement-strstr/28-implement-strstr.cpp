2ef4180aa79ce8e7f267b01ba3dd63fcfd712558
Amanjot singh Batth <89701633+ajbatth@users.noreply.github.com>
2022-01-08 18:49:38 +0530

    Time: 3 ms (98.27%), Space: 7.5 MB (25.69%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,17 +1,47 @@
 class Solution {
 public:
+    vector<int> lps(string p){
+        vector<int>pi(p.size(),0);
+        int j=1;
+        for(int i=1;i<p.size();i++){
+            j=pi[i-1];
+            
+           while(j>0 && p[i]!=p[j]){
+               j=pi[j-1];
+           } 
+            
+            if(p[i]==p[j])
+                j++;
+            
+            pi[i]=j;
+        }
+        return pi;
+    }
+    
     int strStr(string haystack, string needle) {
-        int ns=needle.length();
-        int idx=-1;
         if(needle=="")return 0;
-        if(haystack=="")return -1;
-        if(needle.size()>haystack.size()) return -1;
-        for(int i=0;i<=haystack.length()-ns;i++){
-            if(haystack.substr(i,ns)==needle){
-                idx=i;
+        int pos=-1;
+       vector<int>pi=lps(needle);
+        int i=0,j=0;
+        while(i<haystack.size()){
+            if(haystack[i]==needle[j]){
+                i++;
+                j++;
+            }
+            else{
+                if(j!=0){
+                    j=pi[j-1];
+                }
+                else{
+                    i++;
+                }
+            }
+            
+            if(j==needle.size()){
+                pos=i-needle.size();
                 break;
             }
         }
-        return idx;
+        return pos;
     }
 };
