62d7aa69e091ab189ad734c62eee9400c0491176
Amanjot singh Batth <89701633+ajbatth@users.noreply.github.com>
2022-01-17 13:16:54 +0530

    Time: 7 ms (25.16%), Space: 7.7 MB (78.48%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,5 +1,6 @@
 class Solution {
 private:
+    // using 2 data structures.
     void perm(vector<int>& nums,vector<vector<int>>&ans,int idx,vector<int>&p,vector<int>&map){
         if(p.size()==nums.size()){
             ans.push_back(p);
@@ -14,11 +15,25 @@ private:
             p.pop_back();
         }
     }
+    // without using any additional DS
+void permutation(vector<int>& nums,vector<vector<int>>&ans,int idx){
+    int n=nums.size();
+    if(idx==n){
+        ans.push_back(nums);
+        return;
+    }
+    for(int i=idx;i<n;i++){
+        swap(nums[i],nums[idx]);
+        permutation(nums,ans,idx+1);
+        swap(nums[i],nums[idx]);
+    }
+}
+    
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<int>p,map(nums.size(),0);
         vector<vector<int>>ans;
-        perm(nums,ans,nums.size()-1,p,map);
+        permutation(nums,ans,0);
         return ans;
     }
 };
