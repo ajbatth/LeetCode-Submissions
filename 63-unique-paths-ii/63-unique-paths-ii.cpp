1382ed8eaa8261327ca7dbd2541bfe8587ffa48c
Amanjot singh Batth <89701633+ajbatth@users.noreply.github.com>
2022-01-21 16:58:21 +0530

    Time: 4 ms (67.49%), Space: 7.8 MB (37.61%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -19,6 +19,25 @@ public:
         int m=(int)obstacleGrid.size();
         int n=(int)obstacleGrid[0].size();
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return paths(obstacleGrid,m-1,n-1,dp);
+        if(obstacleGrid[0][0]!=1) dp[0][0]=1;
+        else dp[0][0]=0;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+                int right=0,down=0;
+                if(j>0)right=dp[i][j-1];
+                if(i>0)down=dp[i-1][j];
+                
+                dp[i][j]=right+down;
+                
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
 };
