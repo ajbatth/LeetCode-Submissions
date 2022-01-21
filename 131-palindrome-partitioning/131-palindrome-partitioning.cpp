e09fca88e457298b5f8fb7e2370aecff7ee05c8b
Amanjot singh Batth <89701633+ajbatth@users.noreply.github.com>
2022-01-21 21:07:06 +0530

    Time: 162 ms (60.79%), Space: 53.7 MB (61.97%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,7 +1,7 @@
 class Solution {
 private:
-    bool check_palin(string s){
-            int i=0,j=(s.length()-1);
+    bool check_palin(string s,int start,int end){
+            int i=start,j=end;
         while(i<j){
             if(s[i]!=s[j])return false;
             i++;j--;
@@ -9,28 +9,36 @@ private:
         return true;
     }
     
-    void part(string s,vector<string>&pp,vector<vector<string>>&ans){
+    void part(string s,int st,vector<string>&pp,vector<vector<string>>&ans,vector<vector<int>>&dp){
         int n=s.length();
-        if(n==0){
+        if(st==n){
             ans.push_back(pp);
             return;
         }
         
-        for(int i=0;i<n;i++){
-            string s1=s.substr(0,i+1);
-            string s2=s.substr(i+1);
-            if(check_palin(s1)){
+        for(int i=st;i<n;i++){
+            
+            bool check=0;
+            if(dp[st][i]!=-1)check=dp[st][i];
+            else{
+                dp[st][i]=check=check_palin(s,st,i);
+            }
+            if(check){
+                string s1=s.substr(st,i-st+1);
+                
                 pp.push_back(s1);
-                part(s2,pp,ans);
+                part(s,i+1,pp,ans,dp);
                 pp.pop_back();
             }
         }
     }
 public:
     vector<vector<string>> partition(string s) {
+        int n=s.length();
         vector<string>pp;
         vector<vector<string>>ans;
-        part(s,pp,ans);
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        part(s,0,pp,ans,dp);
         return ans;
     }
 };
