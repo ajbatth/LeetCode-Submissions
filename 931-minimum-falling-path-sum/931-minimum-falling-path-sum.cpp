58d7151f05ffd547616d67555355561f873b9ff8
Amanjot singh Batth <89701633+ajbatth@users.noreply.github.com>
2022-01-30 13:08:15 +0530

    Time: 36 ms (7.52%), Space: 9.9 MB (69.88%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -21,16 +21,37 @@ private:
         
         return dp[row][col]= min(up,min(left_dia,right_dia));
     }
-public:
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int mini=999999;
+    
+    int min_path(vector<vector<int>>&matrix){
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        for(int i=0;i<m;i++){
-            mini=min(mini,minpath(matrix,n-1,i,dp));
-            
+        vector<int>pre=matrix[0],curr(m);
+        for(int i=1;i<n;i++){
+            for(int col=0;col<m;col++){
+                int down=99999,left_dia=99999,right_dia=99999;
+        
+                down=matrix[i][col]+pre[col];
+                if(col>0)
+                left_dia=matrix[i][col]+pre[col-1];
+                if(col<m-1)
+                right_dia=matrix[i][col]+pre[col+1];
+
+                curr[col]= min(down,min(left_dia,right_dia));
+            }
+            pre.swap(curr);
         }
-        return mini;
+        return *min_element(pre.begin(),pre.end());
+    }
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        // int mini=999999;
+        // int n=matrix.size();
+        // int m=matrix[0].size();
+//         vector<vector<int>>dp(n,vector<int>(m,-1));
+//         for(int i=0;i<m;i++){
+//             mini=min(mini,minpath(matrix,n-1,i,dp));
+            
+//         }
+        return min_path(matrix);
     }
 };
