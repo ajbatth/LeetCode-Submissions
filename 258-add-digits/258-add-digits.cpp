915a2f384096909061ddb36b8d4ad7acf751b906
Amanjot singh Batth <89701633+ajbatth@users.noreply.github.com>
2022-02-08 11:11:34 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.37%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num>9){
+            string s=to_string(num);
+            int sum=0;
+            for(int i=0;i<s.size();i++){
+                sum+=s[i]-'0';
+            }
+            num=sum;
+        }
+        return num;
+    }
+};
