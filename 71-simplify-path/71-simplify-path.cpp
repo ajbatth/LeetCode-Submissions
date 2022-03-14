8c17b8f955a226bfe456a170099a446dcb47cd98
Amanjot singh Batth <89701633+ajbatth@users.noreply.github.com>
2022-03-14 12:23:59 +0530

    Time: 4 ms (88.94%), Space: 7.7 MB (87.56%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string simplifyPath(string s) {
+        vector<string>v;
+       
+        int n=s.size();
+        string fol="";
+        for(int i=0;i<=n;i++){
+            if((s[i]=='/'&& i!=0&&s[i-1]!='/')||i==n){
+                if(fol==".."&& v.size()!=0)v.pop_back();
+                else if(fol.size()!=0 && fol!=".")
+                v.push_back(fol);
+                fol="";
+            }
+            if(s[i]!='/')fol.push_back(s[i]);
+        }
+        n=v.size();
+        string ans="";
+        for(int i=0;i<n;i++){
+            if(v[i]!="." && v[i]!=".."){
+                ans.push_back('/');
+                ans.append(v[i]);
+            }
+        }
+        if(ans.size()==0)ans.push_back('/');
+        return ans;
+    }
+};
