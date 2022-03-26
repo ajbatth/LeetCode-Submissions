bfde9c1840dd2b2d441ab7052a433bad7a9b7244
Amanjot singh Batth <89701633+ajbatth@users.noreply.github.com>
2022-03-26 19:59:01 +0530

    Time: 60 ms (26.29%), Space: 18.3 MB (38.81%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -14,7 +14,20 @@ private:
 public:
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return coin(coins,n-1,amount,dp);
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        for(int a=0;a<=amount;a++){
+            if(a%coins[0]==0)dp[0][a]=1;
+            }
+    
+    for(int i=1;i<n;i++){
+        for(int a=0;a<=amount;a++){
+            int take=0,nottake=0;
+        nottake=dp[i-1][a];
+        if(coins[i]<=a)
+            take=dp[i][a-coins[i]];
+         dp[i][a]= take + nottake;
+        }
+    }
+    return dp[n-1][amount];
     }
 };
