27984bcc1dbaba55ae776aa6d22c4e32794b9452
Amanjot singh Batth <89701633+ajbatth@users.noreply.github.com>
2022-04-18 22:36:01 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.61%) - LeetHub

--- 1006-clumsy-factorial/1006-clumsy-factorial.cpp
+++ 1006-clumsy-factorial/1006-clumsy-factorial.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int clumsy(int n) {
         if(n==1)return 1;
-        vector<long long>v;
+        long long ans=0;
         for(long long i=n;i>0;i-=4){
             long long x=i;
-            if(x==1)v.push_back(-1);
+            if(x==1)ans+=(-1);
             if(x-1>=1){
                 long long p=x*(x-1);
                 p*=(i==n?1:-1);
@@ -15,16 +15,12 @@ public:
                 if(x-1>=1){
                    p+=x-1;
                 }
-                v.push_back(p);
+                ans+=(p);
             }
             
             
         }
-        int N=v.size();
-        long long ans=v[0];
-        for(int i=1;i<N;i++){
-            ans+=v[i];
-        }
+        
         return ans;
         
     }
