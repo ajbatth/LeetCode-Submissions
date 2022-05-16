0472a7501cd974f5368d58f5e2d16307914dc352
Amanjot singh Batth <89701633+ajbatth@users.noreply.github.com>
2022-05-16 11:02:00 +0530

    Time: 7 ms (97.30%), Space: 10.8 MB (51.94%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,27 +1,19 @@
-#include <bits/stdc++.h>
-bool cmp(pair<int,int>&a,pair<int,int>&b){
-        return a.first<b.first;
-    }
 class Solution {
 public:
-    
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>mp;
         int n=nums.size();
-        vector<int> ans(2);
-        vector<pair<int,int>>vp;
+        vector<int>ans(2);
         for(int i=0;i<n;i++){
-            vp.push_back({nums[i],i});
-        }
-        sort(vp.begin(),vp.end(),cmp);
-        int i=0,j=n-1;
-        while(i<j){
-            if(vp[i].first+vp[j].first==target){
-                ans[0]=vp[i].second;
-                ans[1]=vp[j].second;
-                break;
+            int fnd=target-nums[i];
+            if(mp.find(fnd)!=mp.end()){
+                ans[0]=i;
+                ans[1]=mp[fnd];
+                return ans;
             }
-            else if(vp[i].first+vp[j].first<target)i++;
-            else if(vp[i].first+vp[j].first>target)j--;
+            else{
+                mp[nums[i]]=i;
+            }
         }
         return ans;
     }
