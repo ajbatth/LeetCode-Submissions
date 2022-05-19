d260f7a1003f9db7097cb5d5f4b24bd44383bba7
Amanjot singh Batth <89701633+ajbatth@users.noreply.github.com>
2022-05-19 12:02:09 +0530

    Time: 82 ms (63.67%), Space: 48.2 MB (92.97%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,34 +1,15 @@
 class Solution {
-private:
-    bool jump(vector<int>& nums,int idx,vector<int>&dp){
-        int n=nums.size();
-        if(idx>=n-1){
-            if(idx==n-1) return true;
-            return false;
-        }
-        if(dp[idx]!=-1)return dp[idx];
-        
-        bool pick=0;
-        for(int i=1;i<=nums[idx];i++){
-            if(pick)return pick;
-            pick=jump(nums,idx+i,dp);
-        }
-        return dp[idx]= pick;
-    }
-    
-    bool linearJump(vector<int>& nums){
-        int n=nums.size();
-        int i=0,dis=0;
-        for(;i<=dis;i++){
-            dis=max(dis,i+nums[i]);
-            if(dis>=n-1) return true;
-        }
-        return false;
-    }
 public:
     bool canJump(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        return linearJump(nums);
-        
+        int n=nums.size();
+        if(n<=1)return true;
+        int req_jump=0;
+        for(int i=n-2;i>=0;i--){
+            req_jump++;
+            if(nums[i]>=req_jump)req_jump=0;
+            
+        }
+        if(req_jump==0)return true;
+        return false;
     }
 };
