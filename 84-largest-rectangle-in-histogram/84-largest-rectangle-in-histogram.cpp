88b975524ecece2cffc136b980f2fc5437d4d350
Amanjot singh Batth <89701633+ajbatth@users.noreply.github.com>
2022-06-03 16:36:59 +0530

    Time: 191 ms (82.95%), Space: 75.4 MB (90.85%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,47 +1,20 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<int>stk,st;
-        vector<int>left,right;
+        int ans=INT_MIN;
+        stack<int>st;
+        st.push(-1);
+        heights.push_back(0);
         int n=heights.size();
-        
-        //nearest smallest number to the left
         for(int i=0;i<n;i++){
-            while(!stk.empty() && heights[stk.top()]>=heights[i]){
-                stk.pop();
-            }
-            if(stk.empty()){
-                left.push_back(-1);
-                stk.push(i);
-                continue;
-            }
-            left.push_back(stk.top());
-            stk.push(i);
-            
-        } 
-        
-        // Nearesr smallest number to the right
-        for(int i=n-1;i>=0;i--){
-             while(!st.empty() && heights[st.top()]>=heights[i]){
+            while(st.size()>1 && heights[i]<=heights[st.top()]){
+                int ht=heights[st.top()];
                 st.pop();
+                int left=st.top();
+                ans=max(ans,ht*(i-left-1));
             }
-            if(st.empty()){
-                right.push_back(n);
-                st.push(i);
-                continue;
-            }
-            right.push_back(st.top());
             st.push(i);
         }
-        reverse(right.begin(),right.end());
-        int maxi=0;
-        for(int i=0;i<n;i++){
-            maxi=max(maxi,(right[i]-left[i]-1)*heights[i]);
-        }
-        return maxi;
-        
+        return ans;
     }
 };
-
-
-
