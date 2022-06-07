042792cb78fc50e9ac63a8d47e66cdc58e9360f8
Amanjot singh Batth <89701633+ajbatth@users.noreply.github.com>
2022-06-07 12:04:55 +0530

    Time: 3 ms (81.51%), Space: 11.9 MB (89.04%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -10,9 +10,17 @@
  * };
  */
 class Solution {
+    void reversePreOrder(TreeNode* root,int level,vector<int>&v){
+        if(root==NULL)return;
+        if(level==v.size())v.push_back(root->val);
+        reversePreOrder(root->right,level+1,v);
+        reversePreOrder(root->left,level+1,v);
+    }
 public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int>ans;
+        reversePreOrder(root,0,ans);
+        return ans;
         if(root==NULL)return ans;
         queue<TreeNode*>q;
         q.push(root);
