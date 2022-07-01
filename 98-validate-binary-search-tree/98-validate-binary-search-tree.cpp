efef83e174ff020d351d7bf266e9f9b93ac45f5c
Amanjot singh Batth <89701633+ajbatth@users.noreply.github.com>
2022-07-01 15:41:28 +0530

    Time: 17 ms (61.07%), Space: 21.7 MB (30.81%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -9,18 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     bool isValidBST(TreeNode* root, long min, long max) {
+//         if(root==NULL)return 1;
+//         bool flag=1;
+//         if(root->val<=min || root->val>=max)return 0;
+//         bool left=1,right=1;
+//         left=isValidBST(root->left,min,root->val);
+//         right=isValidBST(root->right,root->val,max);
+//         return flag&left&right;
+//     }
+//     bool isValidBST(TreeNode* root){
+//         return isValidBST( root,LONG_MIN,LONG_MAX );
+//     }
+// };
+
 class Solution {
-public:
-    bool isValidBST(TreeNode* root, long min, long max) {
-        if(root==NULL)return 1;
-        bool flag=1;
-        if(root->val<=min || root->val>=max)return 0;
-        bool left=1,right=1;
-        left=isValidBST(root->left,min,root->val);
-        right=isValidBST(root->right,root->val,max);
-        return flag&left&right;
-    }
-    bool isValidBST(TreeNode* root){
-        return isValidBST( root,LONG_MIN,LONG_MAX );
+    TreeNode* prev; // Node to store val from last visited inorder node
+    public:
+    bool isValidBST(TreeNode* root) {
+        if(root==NULL)
+            return true;
+        else
+        {
+            if(!isValidBST(root->left))
+                return false;
+            if(prev!=NULL && root->val <= prev->val)
+                return false;
+            prev=root;
+            
+            return isValidBST(root->right);
+            
+        }
     }
 };
